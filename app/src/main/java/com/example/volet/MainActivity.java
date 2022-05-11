1f62fded430837dedab473cdd51193625947f3d4
Sandun Viduranga <91661670+2019t00419@users.noreply.github.com>
2022-05-11 19:57:07 +0530

    loading screen

--- app/src/main/java/com/example/volet/MainActivity.java
+++ app/src/main/java/com/example/volet/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.volet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Home.class);
+                startActivity(i);
+            }
+        },500);
+    }
+
+    public void launchHome(View v){
+        //launch home page
+        Intent i = new Intent(this, Home.class);
+        startActivity(i);
     }
 }
