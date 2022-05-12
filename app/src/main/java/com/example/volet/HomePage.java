af68de1d340fc573688eb753a690cff57ac72f09
Sandun Viduranga <91661670+2019t00419@users.noreply.github.com>
2022-05-12 19:51:41 +0530

    ui devement

--- app/src/main/java/com/example/volet/HomePage.java
+++ app/src/main/java/com/example/volet/HomePage.java
@@ -2,10 +2,12 @@ package com.example.volet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
+//import android.os.Handler;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
 
@@ -14,6 +16,25 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         getSupportActionBar().hide();
+
+        //collecting data from newData.class
+        String descript="";
+        String amount="";
+        String type="";
+
+        Bundle getData= getIntent().getExtras();
+        if(getData !=null){
+            descript= getData.getString("description");
+            amount= getData.getString("amount");
+            type= getData.getString("type");
+        }
+        //setting texts to relevant data
+        TextView textView32 =findViewById(R.id.textView32);
+        TextView textView33 =findViewById(R.id.textView33);
+        TextView textView34 =findViewById(R.id.textView34);
+        textView32.setText(descript);
+        textView33.setText(amount);
+        textView34.setText(type);
     }
 
     public void addData(View v){
--- app/src/main/java/com/example/volet/newData.java
+++ app/src/main/java/com/example/volet/newData.java
@@ -3,19 +3,58 @@ package com.example.volet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class newData extends AppCompatActivity {
 
+    String Type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_data);
         getSupportActionBar().hide();
+        Button in =findViewById(R.id.income);
+        in.setBackgroundColor(Color.rgb(156,39,176));
+        Button out =findViewById(R.id.expense);
+        out.setBackgroundColor(Color.rgb(211,211,211));
     }
     public void submitData(View v){
-        Intent i = new Intent(newData.this, HomePage.class);
-        startActivity(i);
+        //collecting data from the query
+        EditText des = findViewById(R.id.des);
+        EditText amount = findViewById(R.id.amount);
+
+        String descript=des.getText().toString();
+        String amnt=amount.getText().toString();
+
+        //sending data to home page
+        Intent sendData = new Intent(newData.this,HomePage.class);
+        sendData.putExtra("description",descript);
+        sendData.putExtra("amount",amnt);
+
+        startActivity(sendData);
+        finish();
+
+    }
+    public void expensed(View v){
+        TextView test =findViewById(R.id.test);
+        Button in =findViewById(R.id.income);
+        in.setBackgroundColor(Color.rgb(211,211,211));
+        Button out =findViewById(R.id.expense);
+        out.setBackgroundColor(Color.rgb(156,39,176));
+        test.setText("asdfgsdfsf");
+    }
+    public void incomed(View v){
+        TextView test =findViewById(R.id.test);
+        Button in =findViewById(R.id.income);
+        in.setBackgroundColor(Color.rgb(156,39,176));
+        Button out =findViewById(R.id.expense);
+        out.setBackgroundColor(Color.rgb(211,211,211));
+
+        test.setText("");
     }
 }
